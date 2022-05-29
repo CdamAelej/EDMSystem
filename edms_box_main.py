cb1ce34bc756e306749c17d19ac8f1c6e5a6a082
Adam Celej <celejadam01@gmail.com>
2022-05-29 21:35:04 +0200

    Add files via upload
    Trying to make zaladujPracownikow() works

--- edms_box_main.py
+++ edms_box_main.py
@@ -1,11 +1,14 @@
 import os.path
 import sys
+import database
+import sqlalchemy
 from edms_box import Ui_ECDS_form
-from PyQt5 import QtWidgets, QtCore, QtSql
+from PyQt5 import QtWidgets, QtCore
+from sqlalchemy import *
+from sqlalchemy.orm import *
 
 
-
-#class BDelegat(QtWidgets.QStyledItemDelegate):
+# class BDelegat(QtWidgets.QStyledItemDelegate):
 
 
 class GlowneOkno(QtWidgets.QMainWindow, Ui_ECDS_form):
@@ -14,10 +17,11 @@ class GlowneOkno(QtWidgets.QMainWindow, Ui_ECDS_form):
         self.setupUi(self)
         self.pokazPracownikow_action.triggered.connect(lambda: self.zaladujPracownikow())
 
-    def polaczenieBazy(self):
-        bazaDanych = QtSql.QSqlDatabase("bazafirmy")
-        plik = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bazafirmy.sqlite")
-        bazaDanych.setDatabaseName(plik)
+    # Python mowi propert. Python mondry byc XD
+    """def polaczenieBazy(self):
+        bazaDanych = QtSql.QSqlDatabase("bazafirmy.sqlite")
+        #plik = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bazafirmy.sqlite")
+        bazaDanych.setDatabaseName(bazaDanych)
         if not bazaDanych.open():
             QtWidgets.QMessageBox.critical(
                 None,
@@ -28,10 +32,30 @@ class GlowneOkno(QtWidgets.QMainWindow, Ui_ECDS_form):
                 QtWidgets.QMessageBox.Cancel
             )
             return False
-        return True
+        return True"""
 
-    def zaladujPracownikow(self, str):
-        #polaczenie = self.polaczenieBazy()
+    def zaladujPracownikow(self,):
+        Sesja = sessionmaker(bind=database.engine)
+        sesja = Sesja()
+        sw = self.mdiArea.addSubWindow(self.subwindow)
+        self.subwindow.show()
+        sw.resize(640, 480)
+        tabela = self.tableView
+        tabela.horizontalHeader().setStretchLastSection(True)
+        tabela.setWordWrap(True)
+        tabela.setTextElideMode(QtCore.Qt.ElideLeft)
+        selekcja = select(database.Pracownik)
+        wyniki = engine.connect().execute(selekcja)
+        for wiersz in wyniki:
+            print(wiersz)
+        """for s in sesja.query(database.Pracownik).all():
+            print(str(s.IDpracownika), str(s.Imie), str(s.Nazwisko), str(s.DataUrodzenia), str(s.su))"""
+        #tabela.setItemDelegateForColumn()
+        #model = sesja.query(database.Pracownik).all()
+        #tabela.setModel(model)
+        #tabela.resize(640, 480)
+        #tabela.show()
+        """# polaczenie = self.polaczenieBazy()
         sw = self.mdiArea.addSubWindow(self.subwindow)
         self.subwindow.show()
         sw.resize(640, 480)
@@ -43,9 +67,9 @@ class GlowneOkno(QtWidgets.QMainWindow, Ui_ECDS_form):
         model = QtSql.QSqlQueryModel()
         model.setQuery("SELECT * FROM Pracownicy")
 
-        #dane = database.wyswietlPracownikow()
+        # dane = database.wyswietlPracownikow()
 
-        #self.stworz_menu.addAction()
+        # self.stworz_menu.addAction()"""
 
 
 if __name__ == '__main__':
