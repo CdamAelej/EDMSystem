23dbe3ef9abe1e4c2c654e3d01da12c5a9b75aa2
Adam Celej <celejadam01@gmail.com>
2022-05-29 22:45:59 +0200

    Add files via upload
    Changed tableView to tableWidget and made zaladujPracownika works properly

--- edms_box_main.py
+++ edms_box_main.py
@@ -4,7 +4,7 @@ import database
 import sqlalchemy
 from edms_box import Ui_ECDS_form
 from PyQt5 import QtWidgets, QtCore
-from sqlalchemy import *
+import sqlalchemy as db
 from sqlalchemy.orm import *
 
 
@@ -17,59 +17,28 @@ class GlowneOkno(QtWidgets.QMainWindow, Ui_ECDS_form):
         self.setupUi(self)
         self.pokazPracownikow_action.triggered.connect(lambda: self.zaladujPracownikow())
 
-    # Python mowi propert. Python mondry byc XD
-    """def polaczenieBazy(self):
-        bazaDanych = QtSql.QSqlDatabase("bazafirmy.sqlite")
-        #plik = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bazafirmy.sqlite")
-        bazaDanych.setDatabaseName(bazaDanych)
-        if not bazaDanych.open():
-            QtWidgets.QMessageBox.critical(
-                None,
-                QtWidgets.qApp.tr("Nie mozna otworzyc bazy"),
-                QtWidgets.qApp.tr(
-                    "Nie można stworzyc stabilnego polaczenia z baza danych"
-                ),
-                QtWidgets.QMessageBox.Cancel
-            )
-            return False
-        return True"""
 
     def zaladujPracownikow(self,):
-        Sesja = sessionmaker(bind=database.engine)
-        sesja = Sesja()
         sw = self.mdiArea.addSubWindow(self.subwindow)
         self.subwindow.show()
         sw.resize(640, 480)
-        tabela = self.tableView
-        tabela.horizontalHeader().setStretchLastSection(True)
-        tabela.setWordWrap(True)
-        tabela.setTextElideMode(QtCore.Qt.ElideLeft)
-        selekcja = select(database.Pracownik)
-        wyniki = engine.connect().execute(selekcja)
-        for wiersz in wyniki:
-            print(wiersz)
-        """for s in sesja.query(database.Pracownik).all():
-            print(str(s.IDpracownika), str(s.Imie), str(s.Nazwisko), str(s.DataUrodzenia), str(s.su))"""
-        #tabela.setItemDelegateForColumn()
-        #model = sesja.query(database.Pracownik).all()
-        #tabela.setModel(model)
-        #tabela.resize(640, 480)
-        #tabela.show()
-        """# polaczenie = self.polaczenieBazy()
-        sw = self.mdiArea.addSubWindow(self.subwindow)
-        self.subwindow.show()
-        sw.resize(640, 480)
-        tabela = self.tableView
-        tabela.horizontalHeader().setStretchLastSection(True)
-        tabela.setWordWrap(True)
-        tabela.setTextElideMode(QtCore.Qt.ElideLeft)
-        tabela.setItemDelegateForColumn()
-        model = QtSql.QSqlQueryModel()
-        model.setQuery("SELECT * FROM Pracownicy")
-
-        # dane = database.wyswietlPracownikow()
-
-        # self.stworz_menu.addAction()"""
+        silnik = db.create_engine('sqlite:///bazafirmy.sqlite')
+        polaczenie = silnik.connect()
+        metadane = db.MetaData()
+        pracownicy = db.Table('pracownicy', metadane, autoload=True, autoload_with=silnik)
+        kolejka = db.select([pracownicy])
+        wynikiPomocnik = polaczenie.execute(kolejka)
+        wynikiZestaw = wynikiPomocnik.fetchall()
+        self.tableWidget.setRowCount(2)
+        tabelawiersze = 0
+        for wiersz in wynikiZestaw:
+            print(wiersz[0])
+            self.tableWidget.setItem(tabelawiersze, 0, QtWidgets.QTableWidgetItem(str(wiersz[0])))
+            self.tableWidget.setItem(tabelawiersze, 1, QtWidgets.QTableWidgetItem(str(wiersz[1])))
+            self.tableWidget.setItem(tabelawiersze, 2, QtWidgets.QTableWidgetItem(str(wiersz[2])))
+            self.tableWidget.setItem(tabelawiersze, 3, QtWidgets.QTableWidgetItem(str(wiersz[3])))
+            self.tableWidget.setItem(tabelawiersze, 4, QtWidgets.QTableWidgetItem(str(wiersz[4])))
+            tabelawiersze+=1
 
 
 if __name__ == '__main__':
